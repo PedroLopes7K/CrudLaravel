acf554af7a405d18054ee112740ec241cb3418fc
PedroLopes7K <pedrohenri3601@gmail.com>
2022-06-18 20:26:48 -0300

    Add list games

--- app/Http/Controllers/GamesController.php
+++ app/Http/Controllers/GamesController.php
@@ -2,12 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\game;
 use Illuminate\Http\Request;
 
 class GamesController extends Controller
 {
     public function index()
     {
-        return view('games.index');
+
+        $games = game::all();
+        // dd($games);
+        return view('games.index', ['games' => $games]);
     }
 }
--- resources/views/games/index.blade.php
+++ resources/views/games/index.blade.php
@@ -4,5 +4,31 @@
 
 @section('content')
 <!-- tudo aqui dentro será renderizado no template -->
-<h1>Listagem</h1>
+<h1>List Of Games</h1>
+<br>
+<table class="table">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Name</th>
+      <th scope="col">Category</th>
+      <th scope="col">Year</th>
+      <th scope="col">Price</th>
+    </tr>
+  </thead>
+  <tbody>
+
+    @foreach($games as $game)
+    <tr>
+      <th scope="row">{{ $game->id}}</th>
+      <th scope="row">{{ $game->name}}</th>
+      <th scope="row">{{ $game->category}}</th>
+      <th scope="row">{{ $game->year}}</th>
+      <th scope="row">${{ $game->price}}</th>
+
+    </tr>
+    @endforeach
+  </tbody>
+</table>
+
 @endsection
